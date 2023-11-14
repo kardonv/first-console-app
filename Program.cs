1614640f953b98f67a3da000ee0320673db53f48
Volodymyr <vladimir.vinohradov@outlook.com>
2023-11-14 19:33:00 +0200

    Added new file

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
+            /**
+             * Feature 1
+             */
 
             Console.ReadKey();
         }
